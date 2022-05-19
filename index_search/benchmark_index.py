79c7761e2716a93bececf30068624de978870e61
ToniMaciel <tonialvesmaciel@gmail.com>
2022-05-19 18:34:49 -0300

    fix: ajustes nos scripts

--- index_search/benchmark_index.py
+++ index_search/benchmark_index.py
@@ -6,16 +6,19 @@ def index_file(file_name):
     return (p.stderr.replace('\n', ''))
 
 def main():
-    names = ["english.50MB", "english.100MB", "english.200MB", "english.1024MB", "dna.50MB", "dna.100MB", "dna.200MB"]
-    values = []
-    suffix_text_file = "./dataset/textos/"
-    for f in names:
-        values.append(index_file(suffix_text_file+f))
-    
-    with open("index_values.csv", "w") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(names)
-        writer.writerow(values)
+    if len(sys.argv) == 2:
+        print("O tempo necessário para a indexação foi de ", index_file(sys.argv[1]))
+    else: 
+        names = ["english.50MB", "english.100MB", "english.200MB", "english.1024MB", "dna.50MB", "dna.100MB", "dna.200MB"]
+        values = []
+        suffix_text_file = "./dataset/textos/"
+        for f in names:
+            values.append(index_file(suffix_text_file+f))
+
+        with open("index_values.csv", "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(names)
+            writer.writerow(values)
 
 if __name__ == '__main__':
     main()
